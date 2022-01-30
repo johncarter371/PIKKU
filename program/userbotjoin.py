bef1154f97294d538f3f15fca8e83d075317c0d3
SUKHPAL <86770159+SUKHPAL443@users.noreply.github.com>
2022-01-30 21:58:35 +0530

    Update userbotjoin.py

--- program/userbotjoin.py
+++ program/userbotjoin.py
@@ -1,5 +1,5 @@
 import asyncio
-from driver.marrk import user
+from driver.sukhi import user
 from pyrogram.types import Message
 from pyrogram import Client, filters
 from config import BOT_USERNAME, SUDO_USERS
